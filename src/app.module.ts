90888f546010028576f7722c68a85a8e0cea5de2
SGKwon95 <ssunder7@naver.com>
2024-04-06 17:23:18 +0900

    init database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common'
-
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { BalanceModule } from './balance/balance.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'e-commerce',
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+      dropSchema: true,
+    }),
+    BalanceModule,
+  ],
   controllers: [],
   providers: [],
 })
